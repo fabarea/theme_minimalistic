21e0ad3eac75b3950dd120b823f26b80c4afa660
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-06-10 23:02:32 +0200

    [TASK] Main code clean-up

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,14 +11,14 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Theme Demo Collection of configuration / template for this website.',
-	'description' => 'An extension providing a set configuration for this website. Everyone is special... isn\'t it?',
+	'title' => 'Minimalistic fluid theme for reference purposes',
+	'description' => 'A minimalistic fluid theme which serves as reference.',
 	'author' => 'Fabien Udriot',
-	'author_email' => 'support@ecodev.ch',
-	'category' => 'misc',
+	'author_email' => 'fabien.udriot@ecodev.ch',
+	'category' => 'templates',
 	'author_company' => 'Ecodev',
 	'shy' => '',
-	'dependencies' => 'cms,fluidpages,vhs',
+	'dependencies' => 'cms,fluidpages',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -32,10 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 			'cms' => '',
 			'fluidpages' => '',
-			'vhs' => '',
 		),
 		'conflicts' => array(
 		),
@@ -44,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"5f04";s:9:"README.md";s:4:"965c";s:34:"Configuration/TypoScript/setup.txt";s:4:"7a29";s:35:"Resources/Private/Layouts/Page.html";s:4:"a0f3";s:43:"Resources/Private/Partials/PageObjects.html";s:4:"b2e4";s:44:"Resources/Private/Templates/Page/Render.html";s:4:"cf3e";s:49:"Resources/Private/Templates/Page/WithSidebar.html";s:4:"f986";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"1309";}',
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
